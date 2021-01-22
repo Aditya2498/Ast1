aaec2f816b5c42b6d048dfcdca2dea92e56d7f9f
Barsha Singha <khbarshasingha@gmail.com>
2021-01-22 14:40:15 +0530

    productdisplay changed

--- src/Components/Products/ProductDisplay.jsx
+++ src/Components/Products/ProductDisplay.jsx
@@ -8,136 +8,223 @@ import {
   Image,
   ListGroup,
   ListGroupItem,
-  Card
+  Card,
+  Container,
+  Button
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const ProductDisplay = ({ match }) => {
   const key_id = match.params.k_id;
   if (key_id === "1") {
     const data = WaterSealData.find(d => d.id === match.params.id);
     return (
-      <div
-        className="row  "
-        style={{
-          backgroundColor: "rgb(72, 105, 155)",
-
-          borderRadius: "1.5rem"
-        }}
-        height="auto"
-      >
-        <Image src={data.img} alt="image " width="300rem" fluid />
-
-        <ListGroup variant="flush">
-          <ListGroupItem
-            style={{
-              backgroundColor: "gray",
-              color: "white"
-            }}
-          >
-            <strong> Title: {data.Productname} </strong>
-          </ListGroupItem>
-          <ListGroupItem
-            style={{
-              backgroundColor: "gray",
-              color: "white"
-            }}
-          >
-            <strong>Application: {data.Content.Application}</strong>
-          </ListGroupItem>
-          <ListGroupItem
-            style={{
-              backgroundColor: "gray",
-              color: "white"
-            }}
-          >
-            <strong>Features: {data.Content.Features}</strong>
-          </ListGroupItem>
-          <ListGroupItem
-            style={{
-              backgroundColor: "gray",
-              color: "white"
-            }}
-          >
-            <strong>
-              Operating Conditions: {data.Content.OperatingConditions}
-            </strong>
-          </ListGroupItem>
-          <ListGroupItem
-            style={{
-              backgroundColor: "gray",
-              color: "white"
-            }}
-          >
-            <strong>Pressure:{data.Content.Pressure}</strong>
-          </ListGroupItem>
-          <ListGroupItem
-            style={{
-              backgroundColor: "gray",
-              color: "white"
-            }}
-          >
-            <strong> AvailableSize:{data.Content.AvailableSize}</strong>
-          </ListGroupItem>
-        </ListGroup>
-      </div>
+      <Container>
+        <Row style={{ backgroundColor: "rgb(72, 105, 155)", padding: "2rem" }}>
+          <Link to={`/products/${key_id}`}>
+            <Button>back</Button>
+          </Link>
+          <Col sm={12} md={5} lg={4}>
+            <Card>
+              <img className="single-image" src={data.img} alt="image" />
+            </Card>
+          </Col>
+          <Col sm={12} md={7} lg={6}>
+            <ListGroup variant="flush">
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong> Title: {data.Productname} </strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong>Application: {data.Content.Application}</strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong>Features: {data.Content.Features}</strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong>
+                  Operating Conditions: {data.Content.OperatingConditions}
+                </strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong>Pressure:{data.Content.Pressure}</strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong> AvailableSize:{data.Content.AvailableSize}</strong>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     );
   } else if (key_id === "2") {
     const data = ChemicalSealData.find(d => d.id === match.params.id);
     return (
-      <div className="center">
-        <img src={data.img} alt="image" className="center" />
-        <div className="box">
-          <div className="content">
-            <p>
-              <b>Title:{data.Productname}</b>
-            </p>
-            <p>
-              <b>Application:</b> {data.Content.Application}
-            </p>
-            <p>
-              <b>Features:</b> {data.Content.Features}
-            </p>
-            <p>
-              <b>Operating Conditions:</b> {data.Content.OperatingConditions}
-            </p>
-            <p>
-              <b>Pressure:</b> {data.Content.Pressure}
-            </p>
-            <p>
-              <b>AvailableSize:</b> {data.Content.AvailableSize}
-            </p>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <Row style={{ backgroundColor: "rgb(72, 105, 155)", padding: "2rem" }}>
+          <Link to={`/products/${key_id}`}>
+            <Button>back</Button>
+          </Link>
+          <Col sm={12} md={5} lg={4}>
+            <Card>
+              <img className="single-image" src={data.img} alt="image" />
+            </Card>
+          </Col>
+          <Col sm={12} md={7} lg={6}>
+            <ListGroup variant="flush">
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong> Title: {data.Productname} </strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong>Application: {data.Content.Application}</strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong>Features: {data.Content.Features}</strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong>
+                  Operating Conditions: {data.Content.OperatingConditions}
+                </strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong>Pressure:{data.Content.Pressure}</strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong> AvailableSize:{data.Content.AvailableSize}</strong>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     );
   } else if (key_id === "3") {
     const data = OilSealData.find(d => d.id === match.params.id);
     return (
-      <div className="center">
-        <img src={data.img} alt="image" className="center" />
-        <div className="box">
-          <div className="content">
-            <p>
-              <b>Title:{data.Productname}</b>
-            </p>
-            <p>
-              <b>Application:</b> {data.Content.Application}
-            </p>
-            <p>
-              <b>Features:</b> {data.Content.Features}
-            </p>
-            <p>
-              <b>Operating Conditions:</b> {data.Content.OperatingConditions}
-            </p>
-            <p>
-              <b>Pressure:</b> {data.Content.Pressure}
-            </p>
-            <p>
-              <b>AvailableSize:</b> {data.Content.AvailableSize}
-            </p>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <Link to={`/products/${key_id}`}>
+          <Button>back</Button>
+        </Link>
+        <Row style={{ backgroundColor: "rgb(72, 105, 155)", padding: "2rem" }}>
+          <Col sm={12} md={5} lg={4}>
+            <Card>
+              <img className="single-image" src={data.img} alt="image" />
+            </Card>
+          </Col>
+          <Col sm={12} md={7} lg={6}>
+            <ListGroup variant="flush">
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong> Title: {data.Productname} </strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong>Application: {data.Content.Application}</strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong>Features: {data.Content.Features}</strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong>
+                  Operating Conditions: {data.Content.OperatingConditions}
+                </strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong>Pressure:{data.Content.Pressure}</strong>
+              </ListGroupItem>
+              <ListGroupItem
+                style={{
+                  backgroundColor: "rgb(72, 105, 155)",
+                  color: "white"
+                }}
+              >
+                <strong> AvailableSize:{data.Content.AvailableSize}</strong>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 };
